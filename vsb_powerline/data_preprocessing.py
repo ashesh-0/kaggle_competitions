516406116be2d7d8ed22296fa75f09a11b659e98
ashesh <ashesh276@gmail.com>
2019-02-25 19:56:41 +0530

    added peak statistics

--- vsb_powerline/data_preprocessing.py
+++ vsb_powerline/data_preprocessing.py
@@ -1,7 +1,10 @@
 from multiprocessing import Pool
-
+from scipy.signal import find_peaks
+import numpy as np
 import pandas as pd
 import pyarrow.parquet as pq
+
+import numpy as np
 
 
 class DataPipeline:
@@ -61,6 +64,7 @@ class DataProcessor:
             self,
             intended_time_steps: int,
             original_time_steps: int,
+            peak_threshold: int,
             smoothing_window: int = 10,
             num_processes: int = 7,
     ):
@@ -71,6 +75,7 @@ class DataProcessor:
         # with 10, smoothened version seems to have some signals as well.
         self._smoothing_window = smoothing_window
         self._num_processes = num_processes
+        self._peak_threshold = peak_threshold
 
     def get_noise(self, X_df: pd.DataFrame):
         """
@@ -85,6 +90,54 @@ class DataProcessor:
         return noise_df
 
     @staticmethod
+    def peak_stats(ser: pd.Series, threshold, quantiles=[0, 0.25, 0.5, 0.75, 1]):
+        """
+        Returns quantiles of peak width, height, distances from next peak.
+        """
+        peak_indices, data_dict = find_peaks(ser, threshold=threshold, width=1)
+        if len(peak_indices) == 0:
+            # no peaks
+            width_stats = [0] * len(quantiles)
+            height_stats = [0] * len(quantiles)
+            distance_stats = [0] * len(quantiles)
+        else:
+            peak_distances = np.diff(peak_indices)
+
+            peak_width = data_dict['widths']
+            peak_width[peak_width > 100] = 100
+
+            peak_height = data_dict['prominences']
+
+            width_stats = np.quantile(peak_width, quantiles)
+            height_stats = np.quantile(peak_height, quantiles)
+
+            # for just one peak, distance will be empty array.
+            if len(peak_distances)==0:
+                assert len(peak_indices) ==1
+                distance_stats = [ser.shape[0]]* len(quantiles)
+            else:
+                distance_stats = np.quantile(peak_distances, quantiles)
+
+        width_names = ['peak_width_' + str(i) for i in quantiles]
+        height_names = ['peak_height_' + str(i) for i in quantiles]
+        distance_names = ['peak_distances_' + str(i) for i in quantiles]
+
+        index = width_names + height_names + distance_names + ['peak_count']
+        data = np.concatenate([width_stats, height_stats, distance_stats, [len(peak_indices)]])
+
+        return pd.Series(data, index=index)
+
+    @staticmethod
+    def get_peak_stats_df(df, peak_threshold):
+        """
+        Args:
+            df:
+                columns are different examples.
+                axis is time series.
+        """
+        return df.apply(lambda x: DataProcessor.peak_stats(x, peak_threshold), axis=0)
+
+    @staticmethod
     def pandas_describe(df):
         output_df = df.quantile([0, 0.25, 0.5, 0.75, 1], axis=0)
         output_df.index = list(map(lambda x: 'Quant-' + str(x), output_df.index.tolist()))
@@ -94,7 +147,7 @@ class DataProcessor:
         return pd.concat([output_df, abs_mean_df.T, mean_df.T, std_df.T])
 
     @staticmethod
-    def transform_chunk(signal_time_series_df: pd.DataFrame) -> pd.DataFrame:
+    def transform_chunk(signal_time_series_df: pd.DataFrame, peak_threshold: float) -> pd.DataFrame:
         """
         It sqashes the time series to a single point multi featured vector.
         """
@@ -102,9 +155,10 @@ class DataProcessor:
         # mean, var, percentile.
         # NOTE pandas.describe() is the costliest computation with 95% time of the function.
         metrics_df = DataProcessor.pandas_describe(df)
+        peak_metrics_df = DataProcessor.get_peak_stats_df(df, peak_threshold)
 
         metrics_df.index = list(map(lambda x: 'signal_' + x, metrics_df.index))
-        temp_metrics = [metrics_df]
+        temp_metrics = [metrics_df, peak_metrics_df]
 
         for smoothener in [1, 2, 4, 8, 16, 32]:
             diff_df = df.rolling(smoothener).mean()[::smoothener].diff().abs()
@@ -135,7 +189,7 @@ class DataProcessor:
             e_tm_index = s_tm_index + stepsize
             # NOTE: dask was leading to memory leak.
             #one_data_point = delayed(DataProcessor.transform_chunk)(X_df.iloc[s_tm_index:e_tm_index, :])
-            one_data_point = DataProcessor.transform_chunk(X_df.iloc[s_tm_index:e_tm_index, :])
+            one_data_point = DataProcessor.transform_chunk(X_df.iloc[s_tm_index:e_tm_index, :], self._peak_threshold)
             transformed_data.append(one_data_point)
 
         # transformed_data = dd.compute(*transformed_data, scheduler='processes', num_workers=self._num_processes)
