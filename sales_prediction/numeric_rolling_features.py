0275e879a552ba009ecbfc2c440217b66871346b
ashesh <ashesh276@gmail.com>
2019-05-15 18:51:17 +0800

    rolling features moved to monthly data

--- sales_prediction/numeric_rolling_features.py
+++ sales_prediction/numeric_rolling_features.py
@@ -3,12 +3,12 @@ import pandas as pd
 from sortedcontainers import SortedList
 
 
-def ndays_features(sales_df, col_name, num_days, quantiles):
+def nmonths_features(sales_df, col_name, num_months, quantiles):
     # sales_df is sorted by shop_id, item_id and date.
     # Also, shop_item_group value changes when either the shop or the item changes.
-
+    assert num_months >= 1
     columns = ['sum', 'min', 'max'] + ['{}_q'.format(q) for q in quantiles]
-    columns = ['{}_{}d_{}'.format(col_name, num_days, c) for c in columns]
+    columns = ['{}_{}M_{}'.format(col_name, num_months, c) for c in columns]
 
     # For fast processing
     values = sales_df[col_name].values
@@ -21,28 +21,28 @@ def ndays_features(sales_df, col_name, num_days, quantiles):
     tail_index_position = 0
     head_index_position = -1
     tail_index = indices[tail_index_position]
-    tail_day = sales_df.iloc[0]['days']
+    tail_dbn = sales_df.iloc[0]['date_block_num']
 
     cur_group = sales_df.iloc[0]['shop_item_group']
     for head_index in indices:
         head_group = sales_df.at[head_index, 'shop_item_group']
         head_index_position += 1
-        head_day = sales_df.at[head_index, 'days']
+        head_dbn = sales_df.at[head_index, 'date_block_num']
 
         if head_group != cur_group:
             s_list = SortedList()
             cur_group = head_group
             tail_index = head_index
             tail_index_position = head_index_position
-            tail_day = sales_df.at[head_index, 'days']
-            rolling_sum = values[head_index_position]
-            s_list.add(values[head_index_position])
+            tail_dbn = sales_df.at[head_index, 'date_block_num']
+            rolling_sum = 0
         else:
-            item = values[head_index_position]
-            s_list.add(item)
-            rolling_sum += item
+            if head_index_position >= 1:
+                item = values[head_index_position - 1]
+                s_list.add(item)
+                rolling_sum += item
 
-        while head_day - tail_day > num_days:
+        while head_dbn - tail_dbn > num_months:
 
             value_to_be_removed = values[tail_index_position]
             s_list.remove(value_to_be_removed)
@@ -50,10 +50,13 @@ def ndays_features(sales_df, col_name, num_days, quantiles):
 
             tail_index_position += 1
             tail_index = indices[tail_index_position]
-            tail_day = sales_df.at[tail_index, 'days']
+            tail_dbn = sales_df.at[tail_index, 'date_block_num']
 
-        # compute values on data starting at tail_index_position and ending at head_index_position, both inclusive.
-        quants = [s_list[max(0, int(len(s_list) * q) - 1)] for q in quantiles]
-        output_data[head_index_position, :] = [rolling_sum, s_list[0], s_list[-1]] + quants
+        if len(s_list) == 0:
+            output_data[head_index_position, :] = [0] * output_data.shape[1]
+        else:
+            # compute values on data starting at tail_index_position and ending at head_index_position, both inclusive.
+            quants = [s_list[max(0, int(len(s_list) * q) - 1)] for q in quantiles]
+            output_data[head_index_position, :] = [rolling_sum, s_list[0], s_list[-1]] + quants
 
     return pd.DataFrame(output_data, columns=columns, index=sales_df.index)
