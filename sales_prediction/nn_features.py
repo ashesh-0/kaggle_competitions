ddad0249b749cf49fd1c51fce9a3ddde2867ef42
ashesh <ashesh276@gmail.com>
2019-06-03 11:53:12 +0800

    added nearest neighbor features

---
+++ sales_prediction/nn_features.py
@@ -0,0 +1,196 @@
+"""
+Nearest neighbour features. It should work well for those item_id's which are not present
+in test data. This corresponds to new item_ids.
+I've month, item_id, shop_id, item_category_id. So we can use features
+only based on this.
+"""
+from tqdm import tqdm_notebook
+from sklearn.neighbors import NearestNeighbors
+# from multiprocessing import Pool
+import pandas as pd
+import numpy as np
+DEFAULT_VALUE = -10
+
+
+def get_existing_items(sales_df, date_block_num):
+    """
+    Look at past 6 months excluding this date_block_num
+    """
+    dbns = list(range(date_block_num - 6, date_block_num))
+    return set(sales_df[sales_df.date_block_num.isin(dbns)].item_id.unique())
+
+
+class NNModel:
+    def __init__(self, item_ids_text_features, item_ids, n_neighbors, metric):
+        self._item_ids = np.array(item_ids)
+        self.model = NearestNeighbors(n_neighbors=n_neighbors, metric=metric, n_jobs=1, algorithm='auto')
+        self.model.fit(item_ids_text_features)
+
+    def predict(self, item_id_text_feature):
+        NN_output = self.model.kneighbors(item_id_text_feature)
+        neighs = NN_output[1][0]
+        neighs_dist = NN_output[0][0]
+        return (list(self._item_ids[neighs]), neighs_dist)
+
+
+class NNModels:
+    """
+    Create NN models for each category, date_block_num
+    """
+
+    def __init__(self, items_text_data, sales_df, items_df, n_neighbors):
+        # for each category, learn a nearest neighbour on a rolling basis.
+        self._metric = 'minkowski'
+        self._items_text = items_text_data
+        self._sales = sales_df
+        self._items = items_df
+        self._n_neighbors = n_neighbors
+        self._sales = pd.merge(self._sales.reset_index(), self._items, on='item_id', how='left').set_index('index')
+        self._category_to_item_ids = self._sales.groupby('item_category_id')['item_id'].apply(set).to_dict()
+        self.neighbors = {}
+
+        self.run()
+
+    def run(self):
+        for dbn in range(35):
+            self.neighbors[dbn] = {}
+            item_ids = get_existing_items(self._sales, dbn)
+            for item_category_id in self._category_to_item_ids:
+                self.neighbors[dbn][item_category_id] = None
+                cat_item_ids = list(item_ids.intersection(self._category_to_item_ids[item_category_id]))
+
+                if len(cat_item_ids) == 0:
+                    continue
+
+                X = self._items_text[cat_item_ids]
+                model = NNModel(X, cat_item_ids, min(self._n_neighbors, X.shape[0]), self._metric)
+                self.neighbors[dbn][item_category_id] = model
+
+    # map : item_id_date_block_num => [item_ids]
+    # Pick the latest value
+
+
+def get_item_shop_dbn_id(item_id, shop_id, date_block_num):
+    return item_id * 100 * 35 + shop_id * 35 + date_block_num
+
+
+def get_item_dbn_id(item_id, date_block_num):
+    return item_id * 35 + date_block_num
+
+
+def add_item_shop_dbn_id(df):
+    df['item_shop_dbn_idx'] = get_item_shop_dbn_id(df['item_id'], df['shop_id'], df['date_block_num'])
+
+
+def add_item_dbn_id(df):
+    df['item_dbn_idx'] = get_item_dbn_id(df['item_id'], df['date_block_num'])
+
+
+def get_nn_features_data(X_df, feature_col):
+    item_features = X_df.groupby(['item_id', 'date_block_num'])[[feature_col]].mean().reset_index()
+    shop_item_features = X_df[['item_id', 'shop_id', 'date_block_num', feature_col]].copy()
+
+    item_features['date_block_num'] += 1
+    shop_item_features['date_block_num'] += 1
+
+    # create index
+    add_item_shop_dbn_id(shop_item_features)
+    shop_item_features.set_index('item_shop_dbn_idx', inplace=True)
+    shop_item_features.sort_index(inplace=True)
+
+    add_item_dbn_id(item_features)
+    item_features.set_index('item_dbn_idx', inplace=True)
+    item_features.sort_index(inplace=True)
+    return (shop_item_features, item_features)
+
+
+def get_neighbor_item_ids(
+        date_block_num,
+        item_id,
+        neighbors,
+        items_text_X,
+        item_to_category_id,
+):
+    if not isinstance(item_id, int):
+        import pdb
+        pdb.set_trace()
+    item_category_id = item_to_category_id[item_id]
+    model = neighbors[date_block_num][item_category_id]
+    if model is None:
+        return []
+
+    text_features = items_text_X[item_id:(item_id + 1)]
+
+    neighbor_item_ids, _ = model.predict(text_features)
+    if item_id in neighbor_item_ids:
+        neighbor_item_ids.remove(item_id)
+        # TODO: remove from distance.
+
+    return neighbor_item_ids
+
+
+def _get_one_row_feature(date_block_num, shop_id, shop_item_features, item_features, feature_col, neighbor_item_ids):
+    feature = np.zeros(len(neighbor_item_ids))
+    for i, n_item_id in enumerate(neighbor_item_ids):
+        item_shop_dbn_id = get_item_shop_dbn_id(n_item_id, shop_id, date_block_num)
+        if item_shop_dbn_id in shop_item_features.index:
+            feature[i] = shop_item_features.at[item_shop_dbn_id, feature_col]
+        else:
+            item_dbn_id = get_item_dbn_id(n_item_id, date_block_num)
+            assert item_dbn_id in item_features.index, '{}-{} item_dbn_id not present '.format(
+                n_item_id, date_block_num)
+            feature[i] = item_features.at[item_dbn_id, feature_col]
+
+    return np.mean(feature)
+
+
+def get_one_row_feature(
+        date_block_num,
+        item_id,
+        shop_id,
+        shop_item_features,
+        item_features,
+        neighbors,
+        items_text_X,
+        item_to_category_id,
+        feature_col,
+):
+    neighbor_item_ids = get_neighbor_item_ids(date_block_num, item_id, neighbors, items_text_X, item_to_category_id)
+    if len(neighbor_item_ids) == 0:
+        return DEFAULT_VALUE
+
+    return _get_one_row_feature(date_block_num, shop_id, shop_item_features, item_features, feature_col,
+                                neighbor_item_ids)
+
+
+def set_nn_feature(
+        X_df,
+        feature_col,
+        items_text_data,
+        sales_df,
+        items_df,
+        n_neighbors,
+        n_jobs=1,
+):
+    # create models for getting neighbor item_ids
+    models = NNModels(items_text_data, sales_df, items_df, n_neighbors)
+
+    # get features for item ids.
+    (shop_item_features, item_features) = get_nn_features_data(X_df, feature_col)
+
+    item_to_category_id = items_df.set_index('item_id')['item_category_id'].to_dict()
+
+    feature = np.zeros(X_df.shape[0])
+    for idx, row in tqdm_notebook(X_df.iterrows()):
+        feature[idx] = get_one_row_feature(
+            int(row['date_block_num']),
+            int(row['item_id']),
+            int(row['shop_id']),
+            shop_item_features,
+            item_features,
+            models.neighbors,
+            items_text_data,
+            item_to_category_id,
+            feature_col,
+        )
+    X_df['neighbor_' + feature_col] = feature
---
+++ sales_prediction/tests/test_nn_features.py
@@ -0,0 +1,181 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import pandas as pd
+from nn_features import NNModels, NNModel, add_item_dbn_id, add_item_shop_dbn_id, get_neighbor_item_ids, set_nn_feature
+
+
+def dummy_items():
+    items_text = [
+        '"1C-Bitrix Site Manager - Small Business [PC, Digital Version]"',
+        '"1C-Bitrix Site Manager - Standard [PC, Digital Version]"',
+        '"1C-Bitrix Site Manager - Start (Bitrix) [PC, Digital Version]"',
+        'License 1C-Bitrix Site Manager - Small Business',
+        'License 1C-Bitrix Site Manager - Standard',
+        'License 1C-Bitrix Site Manager - Start',
+        'License 1C-Bitrix Site Manager - Expert',
+        '"FIFA Manager 10 [PC, Digital Version]"',
+        '"FIFA Manager 13 [PC, Digital Version]"',
+        '"FIFA Manager 14 [PC, Digital Version]"',
+    ]
+    items_df = pd.DataFrame([], index=list(range(len(items_text))))
+    items_df['item_name'] = items_text
+    items_df['item_id'] = items_df.index.tolist()
+    items_df['item_category_id'] = [0, 0, 0, 1, 1, 1, 1, 2, 2, 2]
+    return items_df
+
+
+def get_items_and_features():
+    items_df = dummy_items()
+    index_list = items_df.index.tolist()
+    np.random.shuffle(index_list)
+    items_df = items_df.loc[index_list]
+
+    tf = TfidfVectorizer()
+    item_features = tf.fit_transform(items_df.item_name.values)
+    return items_df, item_features
+
+
+def test_NNModel():
+    items_df, item_features = get_items_and_features()
+    model = NNModel(item_features, items_df.item_id.values, 3, 'minkowski')
+    item_ids = items_df.item_id.tolist()
+    i = item_ids.index(7)
+    neighbour_ids, _ = model.predict(item_features[i:i + 1])
+    assert set([7, 8, 9]) == set(neighbour_ids)
+
+
+def test_NNModels_should_create_one_month_lagged_models_of_one_category():
+    columns = ['date_block_num', 'shop_id', 'item_id', 'item_cnt_day']
+    data = [
+        [11, 0, 0, 1.0],
+        [11, 0, 1, 2.0],
+        [11, 0, 3, 1.0],
+        [11, 0, 4, 1.0],
+        [11, 0, 5, 5.0],
+        [11, 0, 8, 1.0],
+        [11, 0, 9, 2.0],
+
+        # 2nd month
+        [12, 0, 2, 1.0],
+        [12, 0, 6, 2.0],
+        [12, 0, 7, 1.0],
+    ]
+    sales_df = pd.DataFrame(data, columns=columns)
+    items_df, item_features = get_items_and_features()
+    models = NNModels(item_features, sales_df, items_df, 3)
+    neighbors = models.neighbors
+    for dbn in range(35):
+        if dbn not in [12, 13, 14, 15, 16, 17, 18]:
+            assert neighbors[dbn][0] is None
+            assert neighbors[dbn][1] is None
+            assert neighbors[dbn][2] is None
+        else:
+            assert neighbors[dbn][0] is not None
+            assert neighbors[dbn][1] is not None
+            assert neighbors[dbn][2] is not None
+
+        if dbn == 12:
+            assert set(neighbors[dbn][0]._item_ids) == set([0, 1])
+            assert set(neighbors[dbn][1]._item_ids) == set([3, 4, 5])
+            assert set(neighbors[dbn][2]._item_ids) == set([8, 9])
+        if dbn in [13, 14, 15, 16, 17]:
+            assert set(neighbors[dbn][0]._item_ids) == set([0, 1, 2])
+            assert set(neighbors[dbn][1]._item_ids) == set([3, 4, 5, 6])
+            assert set(neighbors[dbn][2]._item_ids) == set([8, 9, 7])
+        if dbn == 18:
+            assert set(neighbors[dbn][0]._item_ids) == set([2])
+            assert set(neighbors[dbn][1]._item_ids) == set([6])
+            assert set(neighbors[dbn][2]._item_ids) == set([7])
+
+
+def test_new_id_computation():
+    df = pd.DataFrame([[955, 25, 12]], columns=['item_id', 'shop_id', 'date_block_num'])
+
+    add_item_shop_dbn_id(df)
+    assert df.iloc[0]['item_shop_dbn_idx'] == 955 * 100 * 35 + 25 * 35 + 12
+
+    add_item_dbn_id(df)
+    assert df.iloc[0]['item_dbn_idx'] == 955 * 35 + 12
+
+
+def test_get_neighbor_item_ids():
+    columns = ['date_block_num', 'shop_id', 'item_id', 'item_cnt_day']
+    data = [
+        [11, 0, 0, 1.0],
+        [11, 0, 1, 2.0],
+        [11, 0, 3, 1.0],
+        [11, 0, 4, 1.0],
+        [11, 0, 5, 5.0],
+        [11, 0, 8, 1.0],
+        [11, 0, 9, 2.0],
+
+        # 2nd month
+        [12, 0, 2, 1.0],
+        [12, 0, 6, 2.0],
+        [12, 0, 7, 1.0],
+    ]
+    sales_df = pd.DataFrame(data, columns=columns)
+    items_df, items_text_X = get_items_and_features()
+    models = NNModels(items_text_X, sales_df, items_df, 4)
+
+    item_to_category_id = items_df.set_index('item_id')['item_category_id'].to_dict()
+    assert {1} == set(get_neighbor_item_ids(12, 0, models.neighbors, items_text_X, item_to_category_id))
+    assert {0} == set(get_neighbor_item_ids(12, 1, models.neighbors, items_text_X, item_to_category_id))
+    assert {0, 1} == set(get_neighbor_item_ids(12, 2, models.neighbors, items_text_X, item_to_category_id))
+
+    assert {4, 5} == set(get_neighbor_item_ids(12, 3, models.neighbors, items_text_X, item_to_category_id))
+    assert {3, 5} == set(get_neighbor_item_ids(12, 4, models.neighbors, items_text_X, item_to_category_id))
+    assert {3, 4} == set(get_neighbor_item_ids(12, 5, models.neighbors, items_text_X, item_to_category_id))
+    assert {3, 4, 5} == set(get_neighbor_item_ids(12, 6, models.neighbors, items_text_X, item_to_category_id))
+
+    assert {8, 9} == set(get_neighbor_item_ids(12, 7, models.neighbors, items_text_X, item_to_category_id))
+    assert {9} == set(get_neighbor_item_ids(12, 8, models.neighbors, items_text_X, item_to_category_id))
+    assert {8} == set(get_neighbor_item_ids(12, 9, models.neighbors, items_text_X, item_to_category_id))
+    for dbn in range(13, 18):
+        assert {1, 2} == set(get_neighbor_item_ids(dbn, 0, models.neighbors, items_text_X, item_to_category_id))
+        assert {0, 2} == set(get_neighbor_item_ids(dbn, 1, models.neighbors, items_text_X, item_to_category_id))
+        assert {0, 1} == set(get_neighbor_item_ids(dbn, 2, models.neighbors, items_text_X, item_to_category_id))
+
+        assert {4, 5, 6} == set(get_neighbor_item_ids(dbn, 3, models.neighbors, items_text_X, item_to_category_id))
+        assert {3, 5, 6} == set(get_neighbor_item_ids(dbn, 4, models.neighbors, items_text_X, item_to_category_id))
+        assert {3, 4, 6} == set(get_neighbor_item_ids(dbn, 5, models.neighbors, items_text_X, item_to_category_id))
+        assert {3, 4, 5} == set(get_neighbor_item_ids(dbn, 6, models.neighbors, items_text_X, item_to_category_id))
+
+        assert {8, 9} == set(get_neighbor_item_ids(dbn, 7, models.neighbors, items_text_X, item_to_category_id))
+        assert {7, 9} == set(get_neighbor_item_ids(dbn, 8, models.neighbors, items_text_X, item_to_category_id))
+        assert {7, 8} == set(get_neighbor_item_ids(dbn, 9, models.neighbors, items_text_X, item_to_category_id))
+
+
+def test_set_nn_feature():
+    feature_col = 'item_cnt_day'
+    n_neighbors = 4
+
+    columns = ['date_block_num', 'shop_id', 'item_id', 'item_cnt_day']
+    data = [
+        [11, 0, 0, 1.0],
+        [11, 0, 1, 2.0],
+        [11, 1, 0, 5.0],
+        # 2nd category
+        [11, 0, 3, 3.0],
+        [11, 0, 4, 1.0],
+        [11, 1, 5, 5.0],
+        # 3rd category
+        [11, 0, 8, 1.0],
+        [11, 0, 9, 2.0],
+
+        # 2nd month
+        [12, 0, 2, 1.0],
+        [12, 0, 6, 2.0],
+        [12, 0, 7, 1.0],
+    ]
+    sales_df = pd.DataFrame(data, columns=columns)
+    sales_df[['item_id', 'shop_id', 'date_block_num']] = sales_df[['item_id', 'shop_id', 'date_block_num']].astype(
+        np.int32)
+    X_df = sales_df.copy()
+    items_df, items_text_data = get_items_and_features()
+
+    set_nn_feature(X_df, feature_col, items_text_data, sales_df, items_df, n_neighbors)
+    assert all(X_df[X_df.date_block_num == 11]['neighbor_item_cnt_day'].unique() == [-10])
+    assert X_df.iloc[8]['neighbor_item_cnt_day'] == (1 + 2) / 2
+    assert X_df.iloc[9]['neighbor_item_cnt_day'] == (3 + 1 + 5) / 3
+    assert X_df.iloc[10]['neighbor_item_cnt_day'] == (2 + 1) / 2
