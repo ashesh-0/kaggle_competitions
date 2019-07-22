f9c5ce0b10b119f9a01de0c8af3ff9df6c9b2521
ashesh <ashesh276@gmail.com>
2019-07-22 22:30:58 +0800

    removed comments

--- predicting_molecular_properties/find_edges.py
+++ predicting_molecular_properties/find_edges.py
@@ -3,21 +3,6 @@ import numpy as np
 from bond_features import get_bond_data
 from tqdm import tqdm
 
-# def get_index_for_false_H_bond(atom_data, atom_col, atom_idx_col, dis, molecule_edges, H_restrict_dict):
-#     if atom_data[atom_col] == 'H':
-#         atom_index = atom_data[atom_idx_col]
-#         if atom_index in H_restrict_dict:
-#             prev_bond = H_restrict_dict[atom_index]
-#             if prev_bond[0] <= dis:
-#                 return len(molecule_edges)
-#             else:
-#                 H_restrict_dict[atom_index] = [dis, len(molecule_edges)]
-#                 return prev_bond[1]
-#         else:
-#             H_restrict_dict[atom_index] = [dis, len(molecule_edges)]
-
-#     return -1
-
 
 def find_edges(structures_df, bond_len_factor=1.1):
     bonds_df = get_bond_data(return_limited=False)[['atom_0', 'atom_1', 'standard_bond_length']]
@@ -40,8 +25,6 @@ def find_edges(structures_df, bond_len_factor=1.1):
     output = []
     for row in tqdm(start_end):
         molecule_edges = []
-        # false_edge_indices = []
-        # H_restrict_dict = {}
         mn = row[0]
         s = row[1]
         e = row[2]
@@ -56,20 +39,8 @@ def find_edges(structures_df, bond_len_factor=1.1):
                 dis = np.sqrt(np.power(bnd_vector, 2).sum())
                 bnd_vector = bnd_vector / dis
                 if dis <= bonds_df.at[(la[at_col], ra[at_col]), 'standard_bond_length'] * bond_len_factor:
-                    # l_del_idx = get_index_for_false_H_bond(la, at_col, ati_col, dis, molecule_edges, H_restrict_dict)
-                    # r_del_idx = get_index_for_false_H_bond(ra, at_col, ati_col, dis, molecule_edges, H_restrict_dict)
-
-                    # if l_del_idx == len(molecule_edges) or r_del_idx == len(molecule_edges):
-                    #     print('continuing')
-                    #     continue
-                    # if l_del_idx != -1:
-                    #     false_edge_indices.append(l_del_idx)
-                    # if r_del_idx != -1:
-                    #     false_edge_indices.append(r_del_idx)
-
                     molecule_edges.append([mn, la[ati_col], ra[ati_col], dis] + bnd_vector.tolist())
 
-        # output += np.delete(np.array(molecule_edges), false_edge_indices, axis=0).tolist()
         output += molecule_edges
 
     edge_df = pd.DataFrame(output, columns=['molecule_name', 'atom_index_0', 'atom_index_1', 'distance', 'x', 'y', 'z'])
