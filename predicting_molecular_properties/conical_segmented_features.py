601b31dbba74cb4b5c566deefe95c526fcdcfb79
ashesh <ashesh276@gmail.com>
2019-08-11 14:57:24 +0800

    conical segmented features

--- predicting_molecular_properties/conical_segmented_features.py
+++ predicting_molecular_properties/conical_segmented_features.py
@@ -29,20 +29,24 @@ def compute_feature_one_molecule(train_data_ai_0: np.array, train_data_ai_1: np.
     train_count = train_data_ai_0.shape[0]
 
     xyz = [0, 1, 2]
+
+    atom_index_idx = 3
+
     bond_vector_0 = train_data_ai_0[:, xyz] - train_data_ai_1[:, xyz]
     bond_vector_0 = bond_vector_0 / np.linalg.norm(bond_vector_0, axis=1).reshape(train_count, 1)
-    # train_data_ai_0 = np.concatenate([train_data_ai_0, bond_vector], axis=1)
 
-    # (#train_df.shape[0],features,replicas)
     A = np.tile(bond_vector_0.T, (atom_count, 1, 1)).T
 
-    C = np.tile(train_data_ai_0[:, xyz].T, (atom_count, 1, 1)).T
+    bond_mid_xyz = (train_data_ai_0[:, xyz] + train_data_ai_1[:, xyz]) / 2
+    C = np.tile(bond_mid_xyz.T, (atom_count, 1, 1)).T
+
     B = np.tile(molecule_data.T, (train_count, 1, 1))
 
     B[:, xyz, :] = B[:, xyz, :] - C[:, xyz, :]
     YiA_distance = np.linalg.norm(B[:, xyz, :], axis=1)
-    # Ignore self atoms by making the distance very large.
-    YiA_distance = np.where(YiA_distance == 0, 1e10, YiA_distance)
+    # Ignore the two atoms of the bond.
+    YiA_distance[np.arange(train_count), train_data_ai_0[:, atom_index_idx].astype(int)] = 1e10
+    YiA_distance[np.arange(train_count), train_data_ai_1[:, atom_index_idx].astype(int)] = 1e10
     # xyz will become unit vectors, features will get normalized by distance. large distance will have very low
     # contributions
     B = B / YiA_distance.reshape(B.shape[0], 1, B.shape[2])
@@ -50,11 +54,11 @@ def compute_feature_one_molecule(train_data_ai_0: np.array, train_data_ai_1: np.
     angle = (A[:, xyz, :] * B[:, xyz, :]).sum(axis=1)
 
     # -3 to 3. 7 divisions.
-    factor = 3
+    factor = 4
     angle = (angle * factor).astype(int)
 
     features = []
-    for region in range(-factor, factor + 1, 1):
+    for region in range(-factor + 1, factor, 1):
         filtr = (angle == region).astype(int)
         filtr = filtr.reshape((B.shape[0], 1, B.shape[2]))
         feature_one_region = (B[:, 3:, :] * filtr).sum(axis=2)
